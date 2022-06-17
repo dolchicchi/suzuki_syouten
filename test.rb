73dbe630b9b2cb22fccd9e9070c1399a00dd45d7
dolchicchi <99771139+dolchicchi@users.noreply.github.com>
2022-06-18 08:22:01 +0900

    修正

--- test.rb
+++ test.rb
@@ -1,64 +1,24 @@
 line_count = gets.to_i
-bingo_card = []
-line_count.times do |i|
-    bingo_card << gets.chomp.split(" ")
-end
-bingo_card.flatten! # 配列を平坦化(繰り返し処理なしで要素を検証するため)
+bingo_card = Array.new(line_count){gets.split}.flatten
 
 winning_words = readlines(chomp: true).slice(1..)
 winning_words.each do |winning_word|
     change_index = bingo_card.index(winning_word)
-    unless change_index.nil?
-        bingo_card[change_index] = 0
-    end
+    bingo_card[change_index] = 0 unless change_index.nil?
 end
 
-bingo_card = bingo_card.each_slice(line_count).to_a # 平坦化したビンゴカードを配列形式へ復元
+bingo_card = bingo_card.each_slice(line_count).to_a # ビンゴカードを配列形式へ
 
-judge_bingo = false
-# 横ラインのビンゴ確認処理
-bingo_card.each do |line_result|
-    if line_result.count(0) == line_count
-        judge_bingo = true
-        break
-    end
-end
+#　縦
+judge_bingo = bingo_card.transpose.any?{|v| v.count(0) == line_count}
 
-# 縦ラインのビンゴ確認処理(未ビンゴ時のみ実行)
-unless judge_bingo
-    bingo_card_column = bingo_card.transpose  # 縦ラインの配列を作成
-    bingo_card_column.each do |column_result|
-        if column_result.count(0) == line_count
-            judge_bingo = true
-            break
-        end
-    end
-end
+# 横
+judge_bingo = bingo_card.any?{|v| v.count(0) == line_count} unless judge_bingo
 
-# 右下がりのビンゴ確認処理(未ビンゴ時のみ実行)
-unless judge_bingo
-    lower_right = []
-    line_count.times do |i|
-        lower_right << bingo_card[i][i] # 右下がりラインの結果を抽出
-    end
-    
-    if lower_right.count(0) == line_count
-        judge_bingo = true
-    end
-end
+# 斜め
+judge_bingo = Array.new(line_count){|i| bingo_card[i][i] }.all?{|v| v == 0} unless judge_bingo
+judge_bingo = Array.new(line_count){|i| bingo_card[i][-i - 1] }.all?{|v| v == 0} unless judge_bingo
 
-# 左下がりのビンゴ確認処理(未ビンゴ時のみ実行)
-unless judge_bingo
-    lower_left = []
-    line_count.times do |i|
-        line_index = line_count - 1 - i
-        lower_left << bingo_card[line_index][i] # 左下がりラインの結果を抽出
-    end
-    
-    if lower_left.count(0) == line_count
-        judge_bingo = true
-    end
-end
 
 if judge_bingo
     puts "yes"
